eec61791720b474796dee6ac6e271a63c48502c9
skooter500 <skooter500@gmail.com>
2020-02-10 22:31:44 +0000

    bla

--- src/ie/tudublin/Cat.java
+++ src/ie/tudublin/Cat.java
@@ -6,19 +6,23 @@ public class Cat extends Animal
 
     public Cat(String name)
     {
-        // Constructor chaining
+        // Constructor chaining to the superclass
         super(name);
         setNumLives(9);        
     }
 
+    // Accessor method for reading the private field
     public int getNumLives() {
         return numLives;
     }
 
+    // Accessor method for writing to the private field
     public void setNumLives(int numLives) {
         this.numLives = numLives;
     }
 
+    // Virtual function
+    // Dynamic function binding
     public void speak()
     {
         System.out.println("Meow!");
